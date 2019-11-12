359c0b1fd13680976f1ca35022e0124d68d83894
bc8c <bc8c@naver.com>
2019-11-12 22:44:19 +0900

    Make a 3org3peer BasicNetwork

--- generate.sh
+++ generate.sh
@@ -20,22 +20,36 @@ if [ "$?" -ne 0 ]; then
 fi
 
 # generate genesis block for orderer
-configtxgen -profile OneOrgOrdererGenesis -outputBlock ./config/genesis.block
+configtxgen -profile ThreeOrgOrdererGenesis -outputBlock ./config/genesis.block
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate orderer genesis block..."
   exit 1
 fi
 
 # generate channel configuration transaction
-configtxgen -profile OneOrgChannel -outputCreateChannelTx ./config/channel.tx -channelID $CHANNEL_NAME
+configtxgen -profile ThreeOrgChannel -outputCreateChannelTx ./config/channel.tx -channelID $CHANNEL_NAME
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate channel configuration transaction..."
   exit 1
 fi
 
 # generate anchor peer transaction
-configtxgen -profile OneOrgChannel -outputAnchorPeersUpdate ./config/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
+configtxgen -profile ThreeOrgChannel -outputAnchorPeersUpdate ./config/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate anchor peer update for Org1MSP..."
+  exit 1
+fi
+
+# generate anchor peer transaction
+configtxgen -profile ThreeOrgChannel -outputAnchorPeersUpdate ./config/Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org2MSP
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate anchor peer update for Org2MSP..."
+  exit 1
+fi
+
+# generate anchor peer transaction
+configtxgen -profile ThreeOrgChannel -outputAnchorPeersUpdate ./config/Org3MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org3MSP
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate anchor peer update for Org3MSP..."
   exit 1
 fi
--- start.sh
+++ start.sh
@@ -12,7 +12,13 @@ export MSYS_NO_PATHCONV=1
 
 docker-compose -f docker-compose.yml down
 
-docker-compose -f docker-compose.yml up -d ca.example.com orderer.example.com peer0.org1.example.com couchdb
+#docker-compose -f docker-compose.yml up -d ca.example.com orderer.example.com peer0.org1.example.com couchdb
+
+docker-compose -f docker-compose.yml up -d orderer.example.com \
+	       ca.org1.example.com peer0.org1.example.com cli_org1 couchdb1 \
+	       ca.org2.example.com peer0.org2.example.com cli_org2 couchdb2 \
+	       ca.org3.example.com peer0.org3.example.com cli_org3 couchdb3 
+
 docker ps -a
 
 # wait for Hyperledger Fabric to start
@@ -22,6 +28,24 @@ export FABRIC_START_TIMEOUT=10
 sleep ${FABRIC_START_TIMEOUT}
 
 # Create the channel
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel create -o orderer.example.com:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
+#docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel create -o orderer.example.com:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
 # Join peer0.org1.example.com to the channel.
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b mychannel.block
+#docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b mychannel.block
+
+# Create the channel
+docker exec cli_org1 peer channel create -o orderer.example.com:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
+sleep 10
+# Join peer0.org1.example.com to the channel.
+docker exec peer0.org1.example.com peer channel join -b /etc/hyperledger/configtx/mychannel.block
+sleep 10
+docker exec peer0.org2.example.com peer channel join -b /etc/hyperledger/configtx/mychannel.block
+sleep 10
+docker exec peer0.org3.example.com peer channel join -b /etc/hyperledger/configtx/mychannel.block
+sleep 10
+docker exec cli_org1 peer chaincode install -n fabcar -v 1.0 -p github.com/fabcar/go
+docker exec cli_org2 peer chaincode install -n fabcar -v 1.0 -p github.com/fabcar/go
+docker exec cli_org3 peer chaincode install -n fabcar -v 1.0 -p github.com/fabcar/go
+docker exec cli_org1 peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n fabcar -v 1.0 -c '{"Args":[]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
+sleep 10
+docker exec cli_org1 peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n fabcar -c '{"function":"initLedger","Args":[]}'
+
